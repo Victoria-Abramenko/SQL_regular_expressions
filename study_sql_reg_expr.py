2e02dbe55b1cd47c9df47d893f6742bea43d7932
Victoria-Abramenko <victoria666abramenko@gmail.com>
2025-06-01 21:31:14 +0300

    Study SQL stepik

--- study_sql_reg_expr.py
+++ study_sql_reg_expr.py
@@ -1655,3 +1655,24 @@
 #     AND buy_step.date_step_end IS NULL
 # ORDER BY
 #     buy_step.buy_id;
+
+
+
+# # __________________________  task 84  _____________________
+# # В таблице city для каждого города указано количество дней, за которые заказ может быть доставлен в этот город (рассматривается только этап Транспортировка). Для тех заказов, которые прошли этап транспортировки, вывести количество дней за которое заказ реально доставлен в город. А также, если заказ доставлен с опозданием, указать количество дней задержки, в противном случае вывести 0. В результат включить номер заказа (buy_id), а также вычисляемые столбцы Количество_дней и Опоздание. Информацию вывести в отсортированном по номеру заказа виде.
+#
+# SELECT buy.buy_id, DATEDIFF(date_step_end, date_step_beg)  AS Количество_дней, IF( DATEDIFF(date_step_end, date_step_beg) > days_delivery, DATEDIFF(date_step_end, date_step_beg) - days_delivery, 0) AS Опоздание
+# FROM
+#     city
+# JOIN
+#     client USING (city_id)
+# JOIN
+#     buy USING (client_id)
+# JOIN
+#     buy_step USING (buy_id)
+# JOIN
+#     step USING (step_id)
+# WHERE
+#     step_id = 3 AND date_step_end IS NOT NULL
+# ORDER BY
+#     buy_id
